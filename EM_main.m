dc7d6e677962cc8be788f7dc2bacc08043e1761f
Alexander-C-Rose <alexander.rose@okstate.edu>
2024-04-06 21:16:25 -0500

    Update EM_main.m
    I think its working but when gam_s has elements equal 0, then this causes NANs to form in calculations that use gam_s.

--- EM_main.m
+++ EM_main.m
@@ -29,21 +29,57 @@ for i = 1:4 % number of class labels
     init_alpha_A(i) = temp/(rA*cA); 
     init_mu_A(i) = (sum(gab(init_A == i)))/temp;    % could maybe use centers?
     temp2 = xA - init_mu_A(i);
-    init_sigma_A{i} = (temp2*transpose(temp2))/(temp-1); % Covariance
+    init_sigma_A(:,:,i) = (temp2*transpose(temp2))/(temp-1); % Covariance
 end
 
 
 
 % EM algorithm
-% A mosaic
-iterations = 10;
-%for i = 1:iterations
+
+% tolerance for when log-likelihood function should stop EM algorithm
+tol = 5;
+% max number of iterations to run through (prevents the 
+% program from never stopping if the tolerance isn't met
+iterations = 50; 
+for j = 1:iterations
+    disp(j);
     % E-step
     for i = 1:4
         % take the multivariate distribution of the observation where rows are
         % data points and columns are the variable
-        gamma_I(:,i) = init_alpha_A(i) * mvnpdf(transpose(xA), init_mu_A(i), init_sigma_A{i});
+        gamma_I(:,i) = init_alpha_A(i) * mvnpdf(transpose(xA), init_mu_A(i), init_sigma_A(:,:,i));
     end
     gamma_I = gamma_I ./ sum(gamma_I, 2);
-%end
+    disp(gamma_I(1:10,:));
+
+    % M-step
+    % new alpha is the number of data points assigned to a label divided by
+    % total data points.
+    gam_s = sum(gamma_I); % This term is used multiple times; precalc. here
+    alpha = gam_s / (rA*cA);
+    mu = (transpose(gamma_I) * transpose(xA)) ./ transpose(gam_s);
+
+    % loop through clusters and calculate the new sigma parameter
+    for i = 1:4
+        temp = transpose(xA) - mu(i,:);
+        sigma(:,:,i) = (transpose(temp) * (gamma_I(:,i) .* temp)) / gam_s(i);
+    end
+    
+    init_sigma_A = sigma;
+    init_mu_A = mu;
+    init_alpha_A = alpha;
+
+    % log-likelihood function for given iteration
+    for i = 1:4
+        log_l(:,i) = alpha(i) * mvnpdf(transpose(xA), mu(i,:), sigma(:,:,i));
+    end
+    log_likelihood(j) = sum(log(sum(log_l)));
+
+    % End the loop if the log_likelihood is within tolerance
+    if j>1 && (abs(log_likelihood(j) - log_likelihood(j-1)) < tol)
+        break;
+    end
+end
+
+
 
